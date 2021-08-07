1e9a76a78deb2e3833cbd414903223ce75d386dd
aslampangestu <aslampangestu@gmail.com>
2021-08-08 00:34:48 +0700

    done layout;

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Layouts/Header";
+import Footer from "./components/Layouts/Footer";
+import SideMenu from "./components/Layouts/SideMenu";
+import Hero from "./components/Hero";
+import Browse from "./components/Browse";
+import Arrived from "./components/Arrived";
+import Clients from "./components/Clients";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Hero/>
+      <Browse/>
+      <Arrived/>
+      <Clients/>
+      <SideMenu/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Layouts/Header.js
@@ -0,0 +1,27 @@
+import Menu from "./Menu.js";
+// import { Link } from "react-router-dom";
+
+function Header({ cart, currentPage }) {
+  return (
+    <header
+      className={`${currentPage !== "details" && "absolute"} w-full z-50 px-4`}
+    >
+      <div className="container mx-auto py-5">
+        <div className="flex flex-stretch items-center">
+          <div className="w-56 items-center flex">
+            {/* <Link to="/"> */}
+              <img
+              src="/img/content/logo.png"
+                alt="Luxspace | Fulfill your house with beautiful furniture"
+              />
+            {/* </Link> */}
+          </div>
+          <div className="w-full"></div>
+          <Menu currentPage={currentPage} cart={cart} />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
